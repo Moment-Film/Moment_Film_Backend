17a5c68f39e0718215a63e44260bb84af51a5cf5
9yuhyeon <kkh2742@gmail.com>
2023-08-01 18:33:30 +0900

    feat : 회원가입 기능 추가
    body : security 적용하지 않은 상태로 PasswordEncoder을 SecurityConfig에 임시 추가
    
    footer :

--- src/main/java/com/team_7/moment_film/MomentFilmApplication.java
+++ src/main/java/com/team_7/moment_film/MomentFilmApplication.java
@@ -2,14 +2,15 @@ package com.team_7.moment_film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MomentFilmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MomentFilmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MomentFilmApplication.class, args);
+    }
 
 }
