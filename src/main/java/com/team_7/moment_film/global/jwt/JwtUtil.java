3da710abbe115d9f01a9d3f70bd3ee00b1490ad2
9yuhyeon <kkh2742@gmail.com>
2023-08-01 15:24:19 +0900

    feat : JWT Util 추가
    body : JWT Util 클래스 추가
    
    footer :

---
+++ src/main/java/com/team_7/moment_film/global/jwt/JwtUtil.java
@@ -0,0 +1,103 @@
+package com.team_7.moment_film.global.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JWT Util")
+@Component
+public class JwtUtil {
+    public static final String HEADER_ACCESS_TOKEN = "accessToken";
+    public static final String HEADER_REFRESH_TOKEN = "refreshToken";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private final Long ACCESS_TOKEN_EXPIRATION = 6 * 60 * 60 * 1000L; // 6시간
+    private final Long REFRESH_TOKEN_EXPIRATION = 12 * 60 * 60 * 1000L; // 12시간
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+
+    // JWT 생성 시 seretKey를 decode 후 Key 객체에 대입
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // JWT(accessToken) 생성 메서드
+    public String createAccessToken(Long id, String username) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(id)) // 토큰 식별자 : UserId
+                        .claim("username", username)
+                        .setExpiration(new Date(date.getTime() + ACCESS_TOKEN_EXPIRATION))
+                        .setIssuedAt(date)
+                        .signWith(key, SignatureAlgorithm.HS256)
+                        .compact();
+    }
+
+    // JWT(refershToken) 생성 메서드
+    public String createRefreshToken(Long id, String username) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(id)) // 토큰 식별자 : UserId
+                        .claim("username", username)
+                        .setExpiration(new Date(date.getTime() + REFRESH_TOKEN_EXPIRATION))
+                        .setIssuedAt(date)
+                        .signWith(key, SignatureAlgorithm.HS256)
+                        .compact();
+    }
+
+    // JWT Bearer SubString 메서드
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith("Bearer")) {
+            return tokenValue.substring(7);
+        }
+        log.error("유효하지 않은 토큰");
+        throw new NullPointerException("유효하지 않은 토큰");
+    }
+
+    // JWT 검증 메서드
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e){
+            log.error("JWT token validation failed, JWT 토큰 유효성 검증에 실패했습니다.");
+            throw new SecurityException("JWT token validation failed, JWT 토큰 유효성 검증에 실패했습니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 토큰의 유효 기간이 만료되었습니다.");
+            throw new ExpiredJwtException(null, null,"Expired JWT token, 토큰의 유효 기간이 만료되었습니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰입니다.");
+            throw new UnsupportedJwtException("Unsupported JWT token, 지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰입니다.");
+            throw new IllegalArgumentException("JWT claims is empty, 잘못된 JWT 토큰입니다.");
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token, 유효하지 않은 JWT 토큰 형식입니다.");
+            throw new MalformedJwtException("Invalid JWT token, 유효하지 않은 JWT 토큰 형식입니다.");
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature, 유효하지 않은 JWT 서명입니다.");
+            throw new SignatureException("Invalid JWT signature, 유효하지 않은 JWT 서명입니다.");
+        }
+    }
+
+    // JWT의 사용자 정보 가져오는 메서드
+    public Claims getUserInfo(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+}
