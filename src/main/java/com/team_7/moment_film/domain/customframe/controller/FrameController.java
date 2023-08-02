2d309d8c394474c238800d8e627d973ae64413bd
yunji <dbswl4342@gmail.com>
2023-08-02 15:43:16 +0900

    feat: 프레임 기능 기본 틀 추가
    body: 프레임 만들기, 조회, 선택기능 구현

---
+++ src/main/java/com/team_7/moment_film/domain/customframe/controller/FrameController.java
@@ -0,0 +1,39 @@
+package com.team_7.moment_film.domain.customframe.controller;
+
+import com.team_7.moment_film.domain.customframe.dto.FrameRequestDto;
+import com.team_7.moment_film.domain.customframe.dto.FrameResponseDto;
+import com.team_7.moment_film.domain.customframe.service.FrameService;
+import com.team_7.moment_film.global.config.UserDetailsImpl;
+import com.team_7.moment_film.global.dto.CustomResponseEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/frame")
+@RequiredArgsConstructor
+public class FrameController {
+
+    private final FrameService frameService;
+
+    //프레임 커스텀하기(등록하기)
+    @PostMapping("")
+    public CustomResponseEntity<FrameResponseDto> createFrame(@RequestBody FrameRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return frameService.createFrame(requestDto, userDetails.getUser());
+    }
+
+    //유저들이 커스텀한 프레임 모두 조회
+    @GetMapping("")
+    public List<FrameResponseDto> getAllFrame(){
+        return frameService.getAllFrame();
+    }
+
+    //커스텀 프레임 선택(적용)하기
+    @PostMapping("/{frameId}")
+    public CustomResponseEntity<FrameResponseDto> selectFrame(@PathVariable Long frameId) {
+        return frameService.selectFrame(frameId);
+    }
+
+}
