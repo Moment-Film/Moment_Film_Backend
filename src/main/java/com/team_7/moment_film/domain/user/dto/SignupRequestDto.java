1991ac7eb9ab591a8bd85d972fef2bd3064c064b
9yuhyeon <kkh2742@gmail.com>
2023-08-02 10:52:57 +0900

    fix : SignupRequestDto import 정리
    body : 사용되지 않는 import 정리
    
    footer :

--- src/main/java/com/team_7/moment_film/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/team_7/moment_film/domain/user/dto/SignupRequestDto.java
@@ -1,9 +1,6 @@
 package com.team_7.moment_film.domain.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
