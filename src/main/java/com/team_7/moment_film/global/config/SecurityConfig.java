82a527da4735c546cbee15579a350f19bc053a59
9yuhyeon <kkh2742@gmail.com>
2023-08-02 15:16:11 +0900

    fix : SecurityConfig에 PasswordEncoder() 추가
    body : UserService에서 의존성 주입이 되고 있지 않아 SecurityConfig에서 빈으로 등록
    
    footer :

--- src/main/java/com/team_7/moment_film/global/config/SecurityConfig.java
+++ src/main/java/com/team_7/moment_film/global/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,6 +25,11 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
     private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // AuthenticationManager 를 생성하는 메서드
     // AuthenticationConfiguration 를 통해서만 AuthenticationManager 가 생성됨
