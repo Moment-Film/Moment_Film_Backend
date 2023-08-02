ab3e6e6cb68c7654c642875107519743c2d09ba4
9yuhyeon <kkh2742@gmail.com>
2023-08-02 10:54:39 +0900

    feat : 로그인 기능 추가
    body : security 적용하여 jwt 토큰 방식 로그인 기능 추가
    
    footer :

--- src/main/java/com/team_7/moment_film/MomentFilmApplication.java
+++ src/main/java/com/team_7/moment_film/MomentFilmApplication.java
@@ -2,11 +2,10 @@ package com.team_7.moment_film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class MomentFilmApplication {
 
     public static void main(String[] args) {
