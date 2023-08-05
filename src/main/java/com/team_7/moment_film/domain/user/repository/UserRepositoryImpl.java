56af0c7338ef1bb0f4c07fbaf54d798b3b513fb4
yunji <dbswl4342@gmail.com>
2023-08-05 21:38:49 +0900

    feat: 팔로워 많은 순 사용자 조회 기능 추가
    body:

--- src/main/java/com/team_7/moment_film/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/team_7/moment_film/domain/user/repository/UserRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.team_7.moment_film.domain.user.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.team_7.moment_film.domain.user.dto.QSearchResponseDto;
+import com.team_7.moment_film.domain.user.dto.PopularUserResponseDto;
 import com.team_7.moment_film.domain.user.dto.SearchResponseDto;
-import com.team_7.moment_film.domain.user.entity.QUser;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+
+import static com.team_7.moment_film.domain.follow.entity.QFollow.follow;
+import static com.team_7.moment_film.domain.user.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom{
@@ -16,9 +19,22 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     @Override
     public List<SearchResponseDto> searchUserByName(String userKeyword){
         List<SearchResponseDto> result = queryFactory
-                .select(new QSearchResponseDto(QUser.user.username))
-                .from(QUser.user)
-                .where(QUser.user.username.like("%" + userKeyword + "%"))
+                .select(Projections.constructor(SearchResponseDto.class,user.username))
+                .from(user)
+                .where(user.username.like("%" + userKeyword + "%"))
+                .fetch();
+        return result;
+    }
+
+    @Override
+    public List<PopularUserResponseDto> getPopularUser(){
+        List<PopularUserResponseDto> result = queryFactory
+                .select(Projections.constructor(PopularUserResponseDto.class, user.id, user.username, follow.follower.count().as("follower")))
+                .from(user)
+                .leftJoin(follow).on(user.id.eq(follow.following.id))
+                .groupBy(user.id)
+                .orderBy(follow.follower.count().desc())
+                .limit(10)
                 .fetch();
         return result;
     }
