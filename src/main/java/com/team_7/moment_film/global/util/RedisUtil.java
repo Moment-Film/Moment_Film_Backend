ae3a4d1f7d473866a22cd99e5a08728600101f25
9yuhyeon <kkh2742@gmail.com>
2023-08-09 17:29:45 +0900

    feat : AccessToken 만료 시 새 JWT 발행
    body : 1. 최초 로그인 시 redis에 refreshToken 저장
           2. 이후 요청에서 accessToken 만료 시 사용자가 함께 제출한 refreshToken 검증
           3. 검증된 refreshToken과 redis에 저장된 최초 refreshToken과 대조
           4. 토큰 값이 일치하고, redis에서 삭제되지 않았다면 유효한 refreshToken이므로 새 토큰 발행(accessToken, refreshToken)
           5. 새로 발급한 refreshToken을 다시 redis에 저장
           6. 새로 발행한 토큰(accessToken, refreshToken)을 header에 담아 클라이언트에 반환
           7. 클라이언트는 이후 요청부터 새 토큰(accessToken, refreshToken)으로 요청
           accessToken, refreshToken이 모두 재발행 되는 구조로 refreshToken의 만료기간이 계속 늘어감.
    
    footer :

--- src/main/java/com/team_7/moment_film/global/util/RedisUtil.java
+++ src/main/java/com/team_7/moment_film/global/util/RedisUtil.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
@@ -12,8 +13,10 @@ public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
 
     // redis 데이터 저장 메서드 (key : username, val: refreshToken, expiredTime : TTL)
-    public void setData(String key, String val, Long expiredTime){
-        redisTemplate.opsForValue().set(key, val, expiredTime, TimeUnit.MILLISECONDS);
+    public void setData(String key, String val, Date expiredTime) {
+        long expirationInSeconds = (expiredTime.getTime() - System.currentTimeMillis()) / 1000;
+        Duration duration = Duration.ofSeconds(expirationInSeconds);
+        redisTemplate.opsForValue().set(key, val, duration);
     }
 
     // redis 데이터 조회 메서드
