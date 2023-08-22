d6173828bdd2f62624659c677299321702de4c11
yunji <dbswl4342@gmail.com>
2023-08-22 22:21:15 +0900

    fix: 매핑되지 않은 속성 오류로그 무시 추가
    body:

--- src/main/java/com/team_7/moment_film/domain/customfilter/dto/FilterMapper.java
+++ src/main/java/com/team_7/moment_film/domain/customfilter/dto/FilterMapper.java
@@ -3,7 +3,8 @@ package com.team_7.moment_film.domain.customfilter.dto;
 import com.team_7.moment_film.domain.customfilter.entity.Filter;
 import com.team_7.moment_film.global.dto.GenericMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FilterMapper extends GenericMapper<FilterResponseDto, Filter> {
 }
--- src/main/java/com/team_7/moment_film/domain/customframe/dto/FrameMapper.java
+++ src/main/java/com/team_7/moment_film/domain/customframe/dto/FrameMapper.java
@@ -3,7 +3,8 @@ package com.team_7.moment_film.domain.customframe.dto;
 import com.team_7.moment_film.domain.customframe.entity.Frame;
 import com.team_7.moment_film.global.dto.GenericMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FrameMapper extends GenericMapper<FrameResponseDto, Frame> {
 }
