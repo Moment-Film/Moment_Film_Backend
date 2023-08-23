65bddc7041c1f1f4f0b7d3566aa27822bc425fd7
9yuhyeon <kkh2742@gmail.com>
2023-08-24 04:30:30 +0900

    fix : 코드 리뷰 후 사용하지 않는 파일 및 주석 정리
    body :
    
    footer :

--- src/main/java/com/team_7/moment_film/domain/user/service/UserService.java
+++ src/main/java/com/team_7/moment_film/domain/user/service/UserService.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -207,7 +208,7 @@ public class UserService {
     }
 
     // 회원탈퇴
-//    @Transactional
+    @Transactional
     public ResponseEntity<ApiResponse> withdrawal(User user, String accessToken) {
         String username = user.getUsername();
         if (user.getProvider().equals("google")) {
