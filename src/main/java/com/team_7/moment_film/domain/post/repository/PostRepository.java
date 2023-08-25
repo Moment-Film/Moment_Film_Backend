b91886f86d32beb510cc4cc2a15b91aa97231508
9yuhyeon <kkh2742@gmail.com>
2023-08-26 04:44:58 +0900

    fix : 게시글 상세 조회 시 InvalidDataAccessApiUsageException 에러 발생
    body : 게시글 상세 조회 메서드에서 PostRepository의 getPost 메서드에 Named Parameter를 사용하고 있으나 해당 매개 변수에 이름을 제공하지 않아 에러 발생
    - getPost 메서드의 매개변수에 @Param("postId") 애너테이션을 적용하여 매개변수 이름을 제공하여 에러 해결
    
    footer :

--- src/main/java/com/team_7/moment_film/domain/post/repository/PostRepository.java
+++ src/main/java/com/team_7/moment_film/domain/post/repository/PostRepository.java
@@ -3,11 +3,11 @@ package com.team_7.moment_film.domain.post.repository;
 import com.team_7.moment_film.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p join fetch p.user u where p.id = :postId")
-    Optional<Post> getPost(Long postId);
+    Optional<Post> getPost(@Param("postId") Long postId);
 }
