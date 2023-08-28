fa1297979870930d2fa56984c6a0a6b66987e799
yunji <dbswl4342@gmail.com>
2023-08-28 17:35:50 +0900

    fix: 변수명 명확하게 수정
    body:

--- src/main/java/com/team_7/moment_film/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/team_7/moment_film/domain/follow/repository/FollowRepository.java
@@ -9,7 +9,6 @@ public interface FollowRepository extends JpaRepository<Follow,Long> {
     List<Follow> findAllByFollowerId(Long UserId); // 팔로잉 리스트
     List<Follow> findAllByFollowingId(Long UserId); // 팔로워 리스트
     boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
-
     void deleteByFollowerIdAndFollowingId(Long followerId, Long followingId);
 
 }
