4929dab114297eecd0270ed40c4d4e379946d0d3
yunji <dbswl4342@gmail.com>
2023-09-07 15:21:41 +0900

    fix: 사용자 검색 기능 수정
    body: pageabledto -> pagecustom과 통합,
    반환타입, 조건절 변경

--- src/main/java/com/team_7/moment_film/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/team_7/moment_film/domain/user/repository/UserRepositoryCustom.java
@@ -1,14 +1,13 @@
 package com.team_7.moment_film.domain.user.repository;
 
-import com.team_7.moment_film.global.dto.PageCustom;
 import com.team_7.moment_film.domain.user.dto.PopularUserResponseDto;
 import com.team_7.moment_film.domain.user.dto.SearchResponseDto;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UserRepositoryCustom {
-        PageCustom<SearchResponseDto> searchUserByName(String userKeyword, Pageable pageable);
-
+        Page<SearchResponseDto> searchUserByName(String userKeyword, Pageable pageable);
         List<PopularUserResponseDto> getPopularUser();
 }
--- src/main/java/com/team_7/moment_film/global/dto/PageCustom.java
+++ src/main/java/com/team_7/moment_film/global/dto/PageCustom.java
@@ -1,18 +1,25 @@
 package com.team_7.moment_film.global.dto;
 
 import lombok.Getter;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
 public class PageCustom<T>{
     private List<T> content;
-    private PageableDto pageableDto;
+    private boolean first; // 시작 페이지 여부
+    private int pageSize; // 페이지 당 데이터 수
+    private int pageNumber; // 페이지 번호
+    private int numberOfElements; // 현재 페이지 데이터 수
+    private long totalElements; // 총 데이터 수
 
-    public PageCustom(List<T> content, Pageable pageable, Long total){
-        this.content = content;
-        this.pageableDto = new PageableDto(new PageImpl<>(content, pageable, total));
+    public PageCustom(Page<T> page){
+        this.content = page.getContent();
+        this.first = page.isFirst();
+        this.pageSize = page.getSize();
+        this.pageNumber = page.getNumber();
+        this.numberOfElements = page.getNumberOfElements();
+        this.totalElements = page.getTotalElements();
     }
 }
