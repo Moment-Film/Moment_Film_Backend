2df5b228bc5d53fefc07670f3d17025a706811a5
GyuhyeonKim <112394164+9yuhyeon@users.noreply.github.com>
2023-09-15 07:33:07 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,39 @@
 IS_GREEN=$(docker ps | grep green) # green 컨테이너가 실행중인지 확인
-DEFAULT_CONF="/etc/nginx/nginx.conf"
+DEFAULT_CONF="/etc/nginx/nginx.conf" # nginx.conf(기본 설정 파일)
 
+# green 컨테이너가 실행 중이 아닌 경우(blue 실행중)
 if [ -z $IS_GREEN ]; then
   echo "### BLUE => GREEN ###"
   
+  echo "4. reload nginx"
+  sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf # nginx.green.conf 파일을 nginx.conf 파일로 복사(수정)
+  #sudo nginx -s reload # nginx 재시작
+  
   echo "1. get green image"
-  docker-compose pull app1
+  docker-compose pull app1 # app1 컨테이너의 이미지를 pull 받음.
 
   echo "2. green container Up"
-  docker-compose up -d app1
+  docker-compose up -d app1 # app1 컨테이너를 백그라운드로 실행.
 
-  while [ 1 = 1 ]; do
+  while [ 1 = 1 ]; do # 무한 루프 실행
     echo "3. green health check..."
-    sleep 10
+    sleep 10 # 10초 대기
 
-    REQUEST=$(curl http://localhost:8080)
-    if [ -n "$REQUEST" ]; then
+    REQUEST=$(curl http://localhost:8080) #localhost:8080 포트로 요청을 보냄.
+    if [ -n "$REQUEST" ]; then # 위의 요청 결과가 반환되면(작동 중)
       echo "health check success"
-      break;
+      break; # 무한 루프 탈출
     fi
   done;
 
-  echo "4. reload nginx"
-  sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
   echo "5. blue container down"
-  docker-compose stop app2
+  docker-compose stop app2 # app2 컨테이너 내림.
 else
   echo "### GREEN => BLUE ###"
+  
+  echo "4. reload nginx" 
+  sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.conf
+  #sudo nginx -s reload
 
   echo "1. get blue image"
   docker-compose pull app2
@@ -46,10 +51,6 @@ else
       break;
     fi
   done;
-
-  echo "4. reload nginx" 
-  sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
 
   echo "5. green container down"
   docker-compose stop app1
